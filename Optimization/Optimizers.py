4da18383a9f84ab6495a9500943afda25ea7ed70
Jeevika <rj.jeevika.chopra36@gmail.com>
2023-11-24 17:52:41 +0100

    Added Optimizer: SGD

---
+++ Optimization/Optimizers.py
@@ -0,0 +1,8 @@
+# defining the class Sgd for Stochastic Gradient Descent
+class Sgd:
+    def __init__(self,learning_rate ):
+        self.learning_rate = learning_rate
+# Ti implement basic gradient descent update scheme
+    def calculate_update(self, weight_tensor, gradient_tensor):
+        updated_weight = weight_tensor - self.learing_rate * gradient_tensor
+        return updated_weight
